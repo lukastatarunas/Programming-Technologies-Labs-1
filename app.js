4a41bd6d1ee7283c3e44808c0c67050950be4747
lukastatarunas <lukasttatarunas@gmail.com>
2019-08-31 16:33:17 +0300

    Passed name and surname to the constructor and finished countLetterA function

--- app.js
+++ app.js
@@ -4,22 +4,50 @@
 // 20173131
 
 class LaboratorinisVienas {
-    constructor() {
-
+    constructor(name, surname) {
+        this.name = name
+        this.surname = surname
     }
 
     main = () => {
-        let n = "Lukas".length
-        let m = "Tatarūnas".length
-        let arr = [];
+        let n = this.name.length
+        let m = this.surname.length
+        let arr = []
+
         for (let i = 0; i < m; i++) {
-            arr.push(new Array(n));
+            arr.push(new Array(n))
         }
-        return arr;
+
+        return arr
+    }
+
+    countLetterA = () => {
+        let count = 0
+        
+        for (let i = 0; i < this.name.length; i++) {
+            if (this.name.charAt(i) === "a") {
+                count++
+            }
+        }
+        
+        for (let i = 0; i < this.surname.length; i++) {
+            if (this.surname.charAt(i) === "a") {
+                count++
+            }
+        }
+        
+        console.log(count)
+
+        return count
+    }
+
+    fillArray = () => {
+
     }
 
 }
 
-const firstLab = new LaboratorinisVienas()
+const firstLab = new LaboratorinisVienas("Lukas", "Tatarūnas")
 
 firstLab.main()
+firstLab.countLetterA()
