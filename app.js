9f8a86e37e21b5bcc409baf35d82a4a1b9a7a975
lukastatarunas <lukasttatarunas@gmail.com>
2019-09-04 18:59:57 +0300

    Done with 7

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-/* 1
+// 2
+/* 
 Lukas Tatarūnas
 PRIf-17/2
-20173131 */
+20173131
+*/
 
 main = () => {
+
+    // 3
     let n = "Lukas".length
     let m = "Tatarūnas".length
     let b = n + m
@@ -13,6 +17,7 @@ main = () => {
         arr.push(new Array(m))
     }
 
+    // 4
     countLetterA = () => {
         let a = 0
         
@@ -48,8 +53,10 @@ main = () => {
 
     fillArray()
 
+    // 5
     console.log(arr)
 
+    // 6
     let averageRows = []
     let averageColumns = []
     let rowSum = 0
@@ -58,7 +65,7 @@ main = () => {
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < m; j++) {
             rowSum += arr[i][j]
-            if (j == m - 1) {
+            if (j === m - 1) {
                 averageRows.push(Number.parseFloat(rowSum / m))
                 rowSum = 0
             }
@@ -70,7 +77,7 @@ main = () => {
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             columnSum += arr[j][i]
-            if (j == n - 1) {
+            if (j === n - 1) {
                 averageColumns.push(Number.parseFloat(columnSum / n))
                 columnSum = 0
             }
@@ -78,6 +85,20 @@ main = () => {
     }
 
     console.log(averageColumns)
+
+    // 7
+    let count = 0
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (averageRows[i] < arr[i][j]) {
+                count++
+            }
+            if (j === m - 1) {
+                console.log(`${i + 1} row has: ${count} numbers bigger than the average!`)
+                count = 0
+            }
+        }
+    }
 }
 
 main()
